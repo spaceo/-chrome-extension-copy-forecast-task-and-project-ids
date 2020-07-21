e554d76e624747469c76cf3b1e6ff0e196f4913b
Mikkel Jakobsen <mikkel@adaptagency.com>
2020-07-21 09:45:51 +0200

    Supporting both my-list and project kanban view

--- popup.js
+++ popup.js
@@ -8,31 +8,43 @@ function copyToClipboard(text) {
     document.body.removeChild(dummy);
 }
 
+function handleTaskId(taskId, tabs, status) {
+    chrome.tabs.sendMessage(tabs[0].id, {data: ''}, function(response) {
+        const description = response.data.replace(/ /g, "-").toLowerCase();
+        const prefix = `${taskId}-${description}`;
+
+        copyToClipboard(prefix);
+
+        status.innerHTML = `Git prefix: <span class="what-was-copied">"${prefix}"</span> has been copied to your clipboard`;
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const status = document.getElementById('status');
+    status.innerHTML = 'No project or task id was found';
+
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         const tabUrl = tabs[0].url;
-        const projectAndTaskIdMatch = tabUrl.match(/project\/(P-[0-9]+)\/[^\/]+\/(T[0-9]+)/);
-
+        const projectAndTaskIdMatch = tabUrl.match(/my-list\/(T[0-9]+)|project\/(P-[0-9]+)\/[^\/]+\/(T[0-9]+)/);
+        console.log(projectAndTaskIdMatch)
         if (
-            !projectAndTaskIdMatch
-            || projectAndTaskIdMatch[1] === undefined
-            || projectAndTaskIdMatch[2] === undefined
+          projectAndTaskIdMatch
+          && projectAndTaskIdMatch[1] !== undefined
+          && projectAndTaskIdMatch[2] === undefined
         ) {
-            status.innerHTML = 'No project or task id was found';
-            return;
+            handleTaskId(projectAndTaskIdMatch[1], tabs, status);
         }
 
-        chrome.tabs.sendMessage(tabs[0].id, {data: ''}, function(response) {
-            const taskId = projectAndTaskIdMatch[2];
-            const description = response.data.replace(/ /g, "-").toLowerCase();
-            const prefix = `${taskId}-${description}`;
+        if (
+          projectAndTaskIdMatch
+          && projectAndTaskIdMatch[1] === undefined
+          && projectAndTaskIdMatch[2] !== undefined
+          && projectAndTaskIdMatch[3] !== undefined
+        ) {
+            handleTaskId(projectAndTaskIdMatch[3], tabs, status);
+        }
 
-            copyToClipboard(prefix);
-
-            status.innerHTML = `Git prefix: <span class="what-was-copied">"${prefix}"</span> has been copied to your clipboard`;
-        });
-
+        return true;
     });
 }, false);
 
